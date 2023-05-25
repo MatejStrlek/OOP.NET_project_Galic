590c1dce60e8eb288f1cf82f4ae2bf35c0d579f0
Matej <matej.galic.ets@gmail.com>
2023-05-25 21:46:29 +0200

    added main form

--- WindowsFormApp/InitForm.cs
+++ WindowsFormApp/InitForm.cs
@@ -1,12 +1,11 @@
 using DAL.DAO;
-using DAL.Repository;
+using WindowsFormApp;
 
 namespace WIndowsFormApp
 {
     public partial class InitForm : Form
     {
-        public static readonly IRepository repo = RepositoryFactory.GetRepository();
-        private const string PATH = @"D:\Algebra\4. semestar\.NET\OOP.NET_FIFA_project\language_and_gender.txt";
+        private const string PATH = "language_and_gender.txt";
         private const char SEPARATOR = ';';
 
         public InitForm()
@@ -53,55 +52,19 @@ namespace WIndowsFormApp
 
         private void btnLanguage_Click(object sender, EventArgs e)
         {
-            if (cbLanguage.SelectedIndex == 0)
-            {
-                MessageBox.Show("You selected English");
-                //implement logic for English
-            }
-            else if (cbLanguage.SelectedIndex == 1)
-            {
-                MessageBox.Show("You selected Croatian");
-                //implement logic for Croatian
-            }
-            else return;
-        }
+            OpenMainForm();
+        }      
 
         private void btnGender_Click(object sender, EventArgs e)
         {
-            if (cbGender.SelectedIndex == 0) //male
-            {
-                try
-                {
-                    List<Team> maleTeams = repo.GetMaleTeams();
-                    MessageBox.Show(maleTeams[0].ToString());
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show(
-                        "Error while loading API endpoint!",
-                        "API Error",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
-            }
-            else if (cbGender.SelectedIndex == 1) //female
-            {
-                try
-                {
-                    List<Team> femaleTeams = repo.GetFemaleTeams();
-                    MessageBox.Show(femaleTeams[0].ToString());
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show(
-                        "Error while loading API endpoint!",
-                        "API Error",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Error);
-                }
+            OpenMainForm();           
+        }
 
-            }
-            else return;
+        private void OpenMainForm()
+        {
+            this.Hide();
+            Form mainForm = new MainForm();
+            mainForm.Show();         
         }
 
         private void InitForm_FormClosing(object sender, FormClosingEventArgs e)
---
+++ WindowsFormApp/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using DAL.DAO;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIndowsFormApp;
+
+namespace WindowsFormApp
+{
+    public partial class MainForm : Form
+    {
+        public static readonly IRepository repo = RepositoryFactory.GetRepository();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            await LoadFemaleTeams();
+            await LoadMaleTeams();
+        }
+
+        private static async Task LoadFemaleTeams()
+        {
+            try
+            {
+                List<Team> femaleTeams = repo.GetFemaleTeams();
+                MessageBox.Show("Loaded succesfuly");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "Error while loading API endpoint!",
+                    "API Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private static async Task LoadMaleTeams()
+        {
+            try
+            {
+                List<Team> maleTeams = repo.GetMaleTeams();
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "Error while loading API endpoint!",
+                    "API Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
