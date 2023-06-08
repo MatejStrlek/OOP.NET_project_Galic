7040ddf2666eb863110edf44cb0de2c0529819d6
Matej <matej.galic.ets@gmail.com>
2023-06-08 19:49:10 +0200

    DAO added Player

---
+++ DAL/DAO/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace DAL.DAO
+{
+    public class Player
+    {
+        public Player(string name, int dressNumber, string position, bool isCaptain, bool isFavorite)
+        {
+            Name = name;
+            DressNumber = dressNumber;
+            Position = position;
+            IsCaptain = isCaptain;
+            IsFavorite = isFavorite;
+        }
+
+        public string Name { get; set; }
+        public int DressNumber { get; set; }
+        public string Position { get; set; }
+        public bool IsCaptain { get; set; }
+        public bool IsFavorite { get; set; }
+
+        public string GetPlayerInfo()
+        {
+            return $"{Name} ({DressNumber}) - {Position} - {(IsCaptain ? "Captain" : "Player")}";
+        }
+
+        public string GetNameForCheckBox()
+        => $"{Name} ({DressNumber})";
+    }
+}
