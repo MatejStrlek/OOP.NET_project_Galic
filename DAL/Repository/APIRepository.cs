6a130509502bb0145ac92a43ff08b3a4dcf227cf
Matej <matej.galic.ets@gmail.com>
2023-06-23 18:26:54 +0200

    fixed bug with player rang lists

--- DAL/Repository/APIRepository.cs
+++ DAL/Repository/APIRepository.cs
@@ -304,7 +304,47 @@ namespace DAL.Repository
 
         public List<Event> GetMalePlayersEvents()
         {
-            throw new NotImplementedException();
+            JArray jsonMalePlayersEvents = new();
+            List<Event> malePlayersEvents = new();
+
+            jsonMalePlayersEvents = GetWebRequestAPIPlayersAndVisitors(API_URL_MALE_PLAYERS + $"{GetFifaCodeMale()}");
+
+            foreach (JObject game in jsonMalePlayersEvents)
+            {
+                JArray homeTeamEvents = (JArray)game["home_team_events"];
+                JArray awayTeamEvents = (JArray)game["away_team_events"];
+
+                if (game["home_team"].Value<string>("code") == GetFifaCodeMale()) 
+                {
+                    foreach (JObject playerEvent in homeTeamEvents)
+                    {
+                        if (playerEvent.Value<string>("type_of_event") == "yellow-card"
+                            || playerEvent.Value<string>("type_of_event") == "goal")
+                        {
+                            malePlayersEvents.Add(new Event(
+                                playerEvent.Value<string>("type_of_event"),
+                                playerEvent.Value<string>("player")
+                            ));
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (JObject playerEvent in awayTeamEvents)
+                    {
+                        if (playerEvent.Value<string>("type_of_event") == "yellow-card"
+                            || playerEvent.Value<string>("type_of_event") == "goal")
+                        {
+                            malePlayersEvents.Add(new Event(
+                                playerEvent.Value<string>("type_of_event"),
+                                playerEvent.Value<string>("player")
+                            ));
+                        }
+                    }
+                }
+            }
+
+            return malePlayersEvents;
         }
 
         private List<Event> ValidEvent(JToken playerEvent)
@@ -325,33 +365,5 @@ namespace DAL.Repository
 
             return validEvent;
         }
-
-        //        JArray jsonMalePlayersData = new();
-        //        JArray jArrayForUnion = new();
-
-        //        jsonMalePlayersData = GetWebRequestAPIPlayers(API_URL_MALE_PLAYERS + $"{GetFifaCodeMale()}");
-
-        //        List<Player> malePlayers = new();
-
-        //            if (jsonMalePlayersData[0]["home_team"].Value<string>("code") == GetFifaCodeMale())
-        //            {
-        //                jArrayForUnion = new JArray((jsonMalePlayersData[0]["home_team_statistics"]["starting_eleven"]).Union((JArray) jsonMalePlayersData[0]["home_team_statistics"]["substitutes"]));
-        //            }
-        //            else
-        //            {
-        //                jArrayForUnion = new JArray((jsonMalePlayersData[0]["away_team_statistics"]["starting_eleven"]).Union((JArray) jsonMalePlayersData[0]["away_team_statistics"]["substitutes"]));
-        //            }
-
-        //foreach (var player in jArrayForUnion)
-        //{
-        //    malePlayers.Add(new Player(
-        //        player.Value<string>("name"),
-        //        player.Value<int>("shirt_number"),
-        //        player.Value<string>("position"),
-        //        player.Value<bool>("captain")
-        //        ));
-        //}
-
-        //return malePlayers;
     }
 }
